efcd0c8b48c1c0344caac5354022e075fab6455c
banghartOSU <banghart@oregonstate.edu>
2019-06-29 08:33:29 -0700

    Added Q0

--- Assignments/Source Code/Assignment_1_Skeleton_Code_Files/Q0.c
+++ Assignments/Source Code/Assignment_1_Skeleton_Code_Files/Q0.c
@@ -11,12 +11,14 @@
 void fooA(int* iptr){
   
      /*Print the value and address of the integer pointed to by iptr*/
+    printf("%d %p\n", *iptr, iptr);
           
      /*Increment the value of the integer pointed to by iptr by 5*/
+    *iptr += 5;
           
      /*Print the address of iptr itself*/
-
-
+    printf("%p\n", &iptr);
+    
     /*Dummy variable, ignore the warning*/
    int c=5;  
 }
@@ -25,28 +27,34 @@ void fooA(int* iptr){
 void fooB(int* jptr){
   
      /*Print the value and address of the integer pointed to by jptr*/
+    printf("%d %p\n", *jptr, jptr);
                
      /*Decrement the address by 1 pointed to by jptr using jptr */
-
-      
+    jptr -= 1;
+    
      /*Print the address of jptr itself*/
+    printf("%p\n", &jptr);
 }
 
 
 int main(){
     
-    /*Declare an integer x and initialize it randomly to a value in [0,10] 
-*/
-    
+    /*Declare an integer x and initialize it randomly to a value in [0,10] */
+    int x = (rand() % 10) + 1;
     /*Print the value and address of x*/
+    printf("%d %p\n",x, &x);
     
     /*Call fooA() with the address of x*/
+    fooA(&x);
     
     /*Print the value of x*/
+    printf("%d\n",x);
     
     /*Call fooB() with the address of x*/
+   fooB(&x);
     
     /*Print the value and address of x*/
+    printf("%d %p\n",x,&x);
  
     
     return 0;
